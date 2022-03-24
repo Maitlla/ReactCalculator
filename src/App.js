9c218e172c1a5ce9d266af981516c30e2ec8e522
ohmelopido@gmail.com <afdm@postal6.academiapostal.es.local>
2022-03-24 13:51:49 +0100

    ReactCalculatorAcabadaSinCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import { useEffect, useState,  useRef} from 'react';
+// Calculadora
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import History from './Components/History/History.jsx';
 
 function App() { // Componente APP()
+  // const asociadas a un hook State
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
   const [result, setResult] = useState(null);
+  const [resultsHistory, setResultsHistory] = useState([]);
+  // const asociada a un hook Ref
   const memory = useRef(0);
 
-  // hook de tipo state `resultsHistory`
-  // se le da valor inicial de array vacío `[]`
-  const [resultsHistory, setResultsHistory] = useState([]);
-
+  // asigno en la const num el valor parseado del input
   function changeFirstNumberHandler(event) {
-    setFirstNumber(parseFloat(event.target.value));
-    
+    const num = parseFloat(event.target.value);
+    // si en num no hay ningún número y saldría NaN se pone el input en 0
+    if (isNaN(num)) {
+      setFirstNumber(0);
+    } else {
+      setFirstNumber(num);
+    }
   }
 
   function changeSecondNumberHandler(event) {
+    // la función global parseFloat toma el parámetro que se le da y retorna un valor numérico decimal
+    const num = parseFloat(event.target.value);
+    if (isNaN(num)) {
+      setSecondNumber(0);
+    } else {
+      setSecondNumber(num);
+    }
+  }
+
+  /*
+  function changeSecondNumberHandler(event) {
     setSecondNumber(parseFloat(event.target.value));
   }
+  */
 
   function addHandler(event) {
     setResult(firstNumber + secondNumber);
@@ -37,11 +55,12 @@ function App() { // Componente APP()
     setResult(firstNumber / secondNumber);
   }
 
+  // pone en 0 los input firstNumber y secondNumber y vacia Resultado (onClick button C)
   function deleteHandler(event) {
     setFirstNumber("0")
     setSecondNumber("0")
     setResult(null);
-   // setSecondNumber("");
+    // setResult("");
   }
 
   function savevalueHandler(event) {
@@ -50,38 +69,45 @@ function App() { // Componente APP()
 
   }
 
+  //[ ] Poner en input `firstNumber` el valor almacenado en `result` (botón MR) resultado guardado
+  // para ello previamente almacenado en (botón M+)
+  // el input `secondNumber` se queda en 0
   function copyvalueHandler(event) {
     setFirstNumber(memory.current);
   }
 
-  //[ ] Poner en `firstNumber` el valor almacenado en `result`
-  //Eliminar el valor de `secondNumber`
-
-
-  useEffect(
+  useEffect( 
     () => {
       console.log("firstNumber state:", firstNumber);
       console.log("secondNumber state:", secondNumber);
       console.log("result state:", result);
-      console.log("M+: ", memory);
-
+      console.log("M+:", memory);
+      console.log("MR:", memory);
     }
   )
 
-
   useEffect(
-    ()=>{
-      if(result!=null){
+    () => {
+      // si resultado no es null 
+      if (result != null) {
         console.log(result)
-      setResultsHistory([...resultsHistory,result]);
-    }
-    },[result]
+        // .from hace una copia del Array resultsHistory
+        const newHistory = Array.from(resultsHistory);
+        // se añaden los resultados en el Array newHistory (copia de Array resultsHistory) 
+        newHistory.push(result)
+        // se pasa newHistory como parámetro de la función setResultsHistory, y esta lo pasa a resultsHistory
+        setResultsHistory(newHistory);
+
+        // setResultsHistory([...resultsHistory, result]); // Dispersión, es lo mismo que las líneas de código anteriores
+      }
+    }, 
+    [result] // useEffect() se ejecuta cuando tenga result
   )
 
   return (
     <>
       <h1>Calculadora</h1>
-      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler} />
       <br />
       <br />
       <input type="text" value={secondNumber} onChange={changeSecondNumberHandler} />
@@ -100,7 +126,8 @@ function App() { // Componente APP()
       <p>Resultado: {result}</p>
       <p>Historial de operaciones</p>
       <br />
-      <History results={resultsHistory}/> 
+      {/* se incorpóra en su salida proporcionando como prop `results` el contenido de `resultsHistory` */}
+      <History results={resultsHistory} />
     </>
   );
 }
